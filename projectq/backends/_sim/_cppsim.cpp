980bf7087604162a09e1ae9518cf6e274924008e
louiisliu <11610522@mail.sustech.edu.cn>
2020-08-29 23:52:32 +0800

    Fix cppsim module

--- projectq/backends/_sim/_cppsim.cpp
+++ projectq/backends/_sim/_cppsim.cpp
@@ -21,7 +21,6 @@
 #if defined(_OPENMP)
 #include <omp.h>
 #endif
-#include "_cppkernels/cudastart.h"
 #include "_cppkernels/simulator.hpp"
 
 namespace py = pybind11;
@@ -64,7 +63,5 @@ PYBIND11_PLUGIN(_cppsim) {
         .def("run", &Simulator::run)
         .def("cheat", &Simulator::cheat)
         ;
-    // Initial Cuda Device
-    initDevice(0);
     return m.ptr();
 }
